c3a643927458b78e9cbb479139e81d3ad3cf661c
louizi nizar <Nizar Louizi>
2025-01-30 02:31:28 +0100

    Fixed Cors Error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,40 @@
-window.onload = async () => {
+window.onload = () => {
     const games = document.getElementById("games");
     games.innerHTML = "";
-
-    try {
-        const response = await fetch('https://www.mmobomb.com/api1/games?platform=all');
-        
-        if (!response.ok) {
-            console.log('Network response was not ok');
-            return;
-        }
-
-        const data = await response.json();
-        for(let i = 0; i < 101 ; i++) {
-            let imag = "./assets/imgs/browser.png";
-            if(data[i].platform === "PC (Windows)"){
-                imag = "./assets/imgs/windows.png"
+    const data = null;
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === this.DONE) {
+            try {
+                const responseData = JSON.parse(this.responseText);
+                for (let i = 0; i < 101; i++) {
+                    let imag = "./assets/imgs/browser.png";
+                    if (responseData[i].platform === "PC (Windows)") {
+                        imag = "./assets/imgs/windows.png";
+                    }
+                    games.innerHTML += `
+                        <div class="game">
+                            <img src="${responseData[i].thumbnail}" class="preview" alt="${responseData[i].title}">
+                            <div class="titleGame">
+                                <h5>
+                                    ${responseData[i].title}
+                                </h5>
+                                <img src="${imag}" alt="">
+                            </div>
+                            <p>${responseData[i].short_description}</p>
+                        </div>
+                    `;
+                }
+            } catch (error) {
+                console.log('Error parsing response:', error);
             }
-            games.innerHTML += `
-                <div class="game">
-                    <img src="${data[i].thumbnail}" class="preview" alt="${data[i].title}">
-                    <div class="titleGame">
-                        <h5>
-                            ${data[i].title}
-                        </h5>
-                        <img src="${imag}" alt="">
-                    </div>
-                    <p>${data[i].short_description}</p>
-                </div>
-            `;
         }
-    } catch (error) {
-        console.log('There was a problem with the fetch operation:', error);
-    }
+    });
+    xhr.open('GET', 'https://mmo-games.p.rapidapi.com/games?platform=all');
+    xhr.setRequestHeader('x-rapidapi-key', '3465e39d57msh2bc3658af9cc507p11359djsnead8d527bedb');
+    xhr.setRequestHeader('x-rapidapi-host', 'mmo-games.p.rapidapi.com');
+    xhr.setRequestHeader('platform', 'all');
+
+    xhr.send(data);
 };
